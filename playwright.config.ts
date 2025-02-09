74f11fe8abe7ac2c80731191061df87ad510ff9a
krishsah01 <157822205+krishsah01@users.noreply.github.com>
2025-02-09 18:11:52 -0500

    playwrite dependencies

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  globalTimeout: 20000,
+  globalTimeout: 10000,
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
